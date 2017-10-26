60b273f231001cd99131c4745a04b41f11a83b6b
Jason Li <lizhansong@hotmail.com>
2017-10-26 13:07:42 +1100

    final builds

---
+++ dist-cc1-fulldefrag/FastAdaptiveRCImmix_x86_64-linux/include/bootloader.h
@@ -0,0 +1,7 @@
+
+const unsigned heap_default_initial_size = 50*1024*1024;
+const unsigned heap_default_maximum_size = 100*1024*1024;
+const char *rvm_version = "Jikes RVM 3.1.4 (revision 4347a5a59c2605076ed3c80b5c5fd609c7ff981e)";
+const char *rvm_configuration = "FastAdaptiveRCImmix";
+const char *rvm_host_configuration = "/home/spss/projects/JikesRVM-3.1.4/build/configs/FastAdaptiveRCImmix.properties";
+const char *rvm_target_configuration = "/home/spss/projects/JikesRVM-3.1.4/build/targets/x86_64-linux.properties";
---
+++ dist-cc1-nodefrag/FastAdaptiveRCImmix_x86_64-linux/include/bootloader.h
@@ -0,0 +1,7 @@
+
+const unsigned heap_default_initial_size = 50*1024*1024;
+const unsigned heap_default_maximum_size = 100*1024*1024;
+const char *rvm_version = "Jikes RVM 3.1.4 (revision 1f20b9a95f8963a04c5d273194da40551b4f464d)";
+const char *rvm_configuration = "FastAdaptiveRCImmix";
+const char *rvm_host_configuration = "/home/spss/projects/JikesRVM-3.1.4/build/configs/FastAdaptiveRCImmix.properties";
+const char *rvm_target_configuration = "/home/spss/projects/JikesRVM-3.1.4/build/targets/x86_64-linux.properties";
